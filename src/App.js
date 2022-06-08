127347aa8b13e04d884e29eb1e6be99bfe3737af
TaDangHuy <tadanghuy15082000@gmail.com>
2022-06-08 05:17:28 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import BookCardList from './components/bookCardList/bookCardList';
+import SearchBox from './components/searchBox/searchBox';
+
+import BOOKS_DATA from './data/booksData';
+
+import Logo from './assets/bookaholic.png';
+
 import './App.css';
-import bookCardList from './components/bookCardList'
-  
-function App() {
-  return (
-    <div className="App">
-      <bookCardList />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchField: '',
+    };
+  }
+
+  handleChange = (e) => this.setState({ searchField: e.target.value });
+
+  render() {
+    const { searchField } = this.state;
+
+    const filteredBooks = BOOKS_DATA.filter((book) =>
+      book.original_title.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <div>
+          <img alt='logo' src={Logo} width='200px' />
+        </div>
+        <SearchBox placeholder='Search' handleChange={this.handleChange} />
+        <BookCardList books={filteredBooks} />
+      </div>
+    );
+  }
 }
 
 export default App;
