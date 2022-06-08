d5c2d1ccd5c018ef4ab52e6f6a80fb060421cc69
TaDangHuy <tadanghuy15082000@gmail.com>
2022-06-08 05:11:51 +0000

    Logo

--- src/components/bookCardList/bookCardList.jsx
+++ src/components/bookCardList/bookCardList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import LazyLoad from 'react-lazyload';
+import ReviewModal from '../review-modal/review-modal.component';
+import './bookCardList.scss';
 
 const BookCardList = (props) => {
   return (
     <div className='book-card-list'>
       {props.books.map((book) => (
         <LazyLoad once key={book.book_id}>
+          <ReviewModal key={book.book_id} book={book} />
         </LazyLoad>
       ))}
     </div>
